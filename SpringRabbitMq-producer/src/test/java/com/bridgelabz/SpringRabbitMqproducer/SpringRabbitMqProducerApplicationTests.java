ce4258bf951ca942e3bca56b65a4c7429482df1d
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-07-09 00:02:00 +0530

    Added: Spring Boot Rabbit MQ Producer And Consumer Projects for sending and receving the messages passing through Queue

---
+++ SpringRabbitMq-producer/src/test/java/com/bridgelabz/SpringRabbitMqproducer/SpringRabbitMqProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.SpringRabbitMqproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitMqProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
